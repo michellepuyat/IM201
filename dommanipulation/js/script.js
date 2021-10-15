518769218e4ea2460e1bbe8f795e750aa345a502
michellepuyat <u540209535@byui.edu>
2021-10-15 13:56:30 +0800

    commit

---
+++ dommanipulation/js/script.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+const input = document.querySelector('button');
+const main = document.querySelector('main');
+
+button.addEventListener('click', function() {
+
+}
