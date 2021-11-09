4643560482ba7dc03a8cd183887ab3d1099616d6
michellepuyat <u540209535@byui.edu>
2021-11-09 16:29:22 +0800

    commit

---
+++ lesson9/js/webfontload.js
@@ -0,0 +1,7 @@
+WebFont.load({
+    google: {
+        families: ['Rubik','Rouge Script'],
+        urls: ['https://fonts.googleapis.com/css2?family=Rouge+Script&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap']
+    },
+    timeout: 2000 // Set the timeout to two seconds
+    });
