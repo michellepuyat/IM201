f28d61cb5cd86f85ffc8e643819d48449f6967e8
michellepuyat <u540209535@byui.edu>
2021-11-17 13:09:23 +0800

    commit

---
+++ lesson10/js/lazy-load.js
@@ -0,0 +1,38 @@
+//find all images that has data-src attribute
+const images = document.querySelectorAll('[data-src]');
+
+function preloadImage(img) {
+    //get the attribute of data-src
+    const src = img.getAttribute('data-src');
+    if (!src) {
+        return;
+    } 
+
+    img.src = src;
+    img.removeAttribute('data-src');
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px 0px 0px'
+};
+
+//create image observer
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    //go over each iamge
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            //stop observing after preload
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions)
+
+//check all images
+images.forEach(image => {
+    //run image observe to observe the image
+    imgObserver.observe(image);
+});
---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,16 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a3d8b9de1fc1f3f0ac9d0f0ad4f61c16';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        console.log(jsObject);
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+
+        document.getElementById('imagesrc').textContent = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
+    });
+
